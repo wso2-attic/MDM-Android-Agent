1ce74cfc64d9a499911076059f850e410e54119a
Niranjan-K <niranjan.karu@gmail.com>
2014-02-04 10:34:54 +0530

    Changed reference in wso2mobile to wso2.mobile

--- src/com/wso2/mobile/mdm/utils/CommonUtilities.java
+++ src/com/wso2/mobile/mdm/utils/CommonUtilities.java
@@ -13,7 +13,7 @@
  ~ See the License for the specific language governing permissions and
  ~ limitations under the License.
 */
-package com.wso2mobile.mdm.utils;
+package com.wso2.mobile.mdm.utils;
 
 import android.content.Context;
 import android.content.Intent;
